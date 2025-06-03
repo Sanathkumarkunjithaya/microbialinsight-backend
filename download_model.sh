6f546fba7e5e48d66f78816b6217eb9d079c6a8f
SANATH <kunjithayas@gmail.com>
2025-06-03 15:44:22 +0530

    Update download_model.sh to place ensemble_model.pkl in model directory

--- download_model.sh
+++ download_model.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-curl -L -o ensemble_model.pkl "https://drive.google.com/uc?export=download&id=1z6xXUwpwxV_KEsBmdevT1-XCDC6xQWGb"
+mkdir -p model
+curl -L -o model/ensemble_model.pkl "https://drive.google.com/uc?export=download&id=NEW_FILE_ID"
