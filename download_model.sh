46624bbb88bdc23fbe67d269b12b3c54bb520c19
SANATH <kunjithayas@gmail.com>
2025-06-03 16:36:44 +0530

    Switch to Dropbox for ensemble_model.pkl download

--- download_model.sh
+++ download_model.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 mkdir -p model
-curl -L -o model/ensemble_model.pkl "https://drive.usercontent.google.com/download?id=14A7ZkGisCGfLMlBcrdIXxURyhDkOKPRo"
+curl -L -o model/ensemble_model.pkl "https://www.dropbox.com/scl/fi/hjxphd2y04u4sp2c6s1hh/ensemble_model.pkl?rlkey=5xi43xojgrqyeywfw4ie15v8b&e=1&st=95aimj8z&dl=1"
